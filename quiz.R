afa12344e56866064520877b0df699ec35ea562d
aidoskanapyanov <aidos.kanapyanov@nu.edu.kz>
2020-05-23 20:27:22 +0600

    Final code

--- quiz.R
+++ quiz.R
@@ -1,2 +1,21 @@
 # Quiz week 3
 
+library(datasets)
+data(iris)
+
+?iris
+mean(iris$Sepal.Length[iris$Species=='virginica'])
+
+apply(iris[,1:4], 2, mean)
+
+library(datasets)
+data(mtcars)
+?mtcars
+
+sapply(split(mtcars$mpg,mtcars$cyl),mean)
+tapply(mtcars$mpg,mtcars$cyl, mean)
+with(mtcars,tapply(mpg, cyl, mean))
+
+sapply(split(mtcars$hp,mtcars$cyl),mean)
+abs(sapply(split(mtcars$hp,mtcars$cyl),mean)['4']-
+    sapply(split(mtcars$hp,mtcars$cyl),mean)['8'])
